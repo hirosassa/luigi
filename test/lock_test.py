96b5187ae0b0f1ae155b550b16c6a68622cc63d6
hirosassa <hiro.sassa@gmail.com>
2021-11-03 19:15:51 +0900

    test

--- test/lock_test.py
+++ test/lock_test.py
@@ -37,7 +37,7 @@ class TestCmd(unittest.TestCase):
             command = ["sleep", "1"]
 
         external_process = subprocess.Popen(command)
-        with open("/proc/{external_process.pid}/cmdline", 'r') as fh:
+        with open(f"/proc/{external_process.pid}/cmdline", 'r') as fh:
             s = fh.read().replace('\0', ' ').rstrip()
             print(f"c: {s}")
         result = luigi.lock.getpcmd(external_process.pid)
